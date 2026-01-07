381aa84b1a3d30ed59b7e0a7279fb4af5722f83a
Tamal Ghosh <162481050+Tamal-Ghosh@users.noreply.github.com>
2026-01-07 23:52:18 +0600

    Initial test

---
+++ src/main/java/org/example/demo/main.java
@@ -0,0 +1,24 @@
+package org.example.demo;
+
+
+import org.json.JSONObject;
+
+public class main {
+
+    public static   void main(String args[]) throws Exception {
+
+        String json = "{\"name\":\"Alice\",\"age\":20}";
+
+//        ObjectMapper mapper = new ObjectMapper();
+//        person p=mapper.readValue(json, person.class);
+//        System.out.println("Name: " + p.name);
+//        System.out.println("Age: " + p.age);
+        JSONObject jsonObject = new JSONObject(json);
+        String name= jsonObject.getString("name");
+        int age= jsonObject.getInt("age");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+
+    }
+
+}
